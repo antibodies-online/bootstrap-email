af5b911c69b7a9346e3cbcd681bb4a4a03faf647
Benedikt Lenzen <36764562+DemigodCode@users.noreply.github.com>
2022-03-22 15:44:59 +0100

    Update Compiler.php

--- src/Compiler.php
+++ src/Compiler.php
@@ -1,129 +1,62 @@
-<?php
+# bootstrap-email
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/DemigodCode/bootstrap-email/master/LICENSE.md)
+[![Packagist](https://img.shields.io/packagist/v/DemigodCode/bootstrap-email.svg)](https://packagist.org/packages/DemigodCode/bootstrap-email)
+[![GitHub issues](https://img.shields.io/github/issues/DemigodCode/bootstrap-email.svg)](https://github.com/DemigodCode/bootstrap-email/issues)
 
-namespace DemigodCode\BootstrapEmail;
+This is a port of [Bootstrap Email](https://github.com/bootstrap-email/bootstrap-email). Thanks to @stuyam!
 
-use DemigodCode\BootstrapEmail\Converter\Alert;
-use DemigodCode\BootstrapEmail\Converter\Align;
-use DemigodCode\BootstrapEmail\Converter\Badge;
-use DemigodCode\BootstrapEmail\Converter\Block;
-use DemigodCode\BootstrapEmail\Converter\Body;
-use DemigodCode\BootstrapEmail\Converter\Button;
-use DemigodCode\BootstrapEmail\Converter\Card;
-use DemigodCode\BootstrapEmail\Converter\Container;
-use DemigodCode\BootstrapEmail\Converter\Grid;
-use DemigodCode\BootstrapEmail\Converter\HeadStyle;
-use DemigodCode\BootstrapEmail\Converter\Hr;
-use DemigodCode\BootstrapEmail\Converter\Image;
-use DemigodCode\BootstrapEmail\Converter\Margin;
-use DemigodCode\BootstrapEmail\Converter\MetaTag;
-use DemigodCode\BootstrapEmail\Converter\Padding;
-use DemigodCode\BootstrapEmail\Converter\PreviewText;
-use DemigodCode\BootstrapEmail\Converter\Spacer;
-use DemigodCode\BootstrapEmail\Converter\Spacing;
-use DemigodCode\BootstrapEmail\Converter\Table;
-use DemigodCode\BootstrapEmail\Converter\VersionComment;
-use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
+## Installation
 
-class Compiler
+To install, add it to your `composer.json` file:
+
+```json
 {
-    private $converters = [];
-    private $configurators = [];
-    private $cssConverters = [];
-    private $scssCompiler;
-
-    public function __construct(ScssCompiler $scssCompiler)
-    {
-        $this->converters[] = new Body();
-        $this->converters[] = new Block();
-
-        $this->converters[] = new Button();
-        $this->converters[] = new Badge();
-        $this->converters[] = new Alert();
-        $this->converters[] = new Card();
-        $this->converters[] = new Hr();
-        $this->converters[] = new Container();
-        $this->converters[] = new Grid();
-//        $this->converters[] = new Stack();
-
-//        $this->converters[] = new Color();
-        $this->converters[] = new Spacing();
-        $this->converters[] = new Margin();
-        $this->converters[] = new Spacer();
-        $this->converters[] = new Align();
-        $this->converters[] = new Padding();
-        $this->converters[] = new PreviewText();
-        $this->converters[] = new Table();
-
-        $this->configurators[] = new HeadStyle($scssCompiler);
-        $this->configurators[] = new MetaTag();
-        $this->configurators[] = new VersionComment();
-
-        $this->cssConverters[] = new Image();
-        $this->scssCompiler = $scssCompiler;
-    }
-
-    public function convertHtml(string $html) {
-        $document = new \DOMDocument('1.0', 'UTF-8');
-        $document->loadHTML(mb_convert_encoding($html));
-        $this->convert($document);
-        return $document->saveHTML();
-    }
-
-    public function convert(\DOMDocument $document): \DOMDocument {
-        $document = $this->addLayout($document);
-
-        $document = $this->compile($document);
-        $document = $this->configure($document);
-        $this->inlineCss($document, $this->scssCompiler->getCss());
-        $document = $this->cssCompile($document);
-        return $document;
-    }
-
-    private function compile(\DOMDocument $document) {
-        foreach($this->converters as $converter) {
-            $document = $converter->convert($document);
-        }
-        return $document;
-    }
-
-    private function configure(\DOMDocument $document) {
-        foreach($this->configurators as $configurator) {
-            $document = $configurator->convert($document);
-        }
-        return $document;
-    }
-
-    private function cssCompile(\DOMDocument $document) {
-        foreach($this->cssConverters as $converter) {
-            $document = $converter->convert($document);
-        }
-        return $document;
-    }
-
-    private function addLayout(\DOMDocument $document) {
-        $xPath = new \DOMXPath($document);
-        if (count($xPath->query('//head')) === 0) {
-            $loader = new FilesystemLoader(__DIR__.'/../resources');
-            $twig = new Environment($loader, ['autoescape' => false]);
-            $template = $twig->load('layout.html');
-            $document->loadHTML(mb_convert_encoding($template->render(['contents' => $document->saveHTML()]), 'HTML-ENTITIES', 'UTF-8'));
-        }
-        return $document;
-    }
-
-    private function inlineCss(\DOMDocument $document, string $css): void {
-        $cssToInline = new CssToInlineStyles();
-        $xPath = new \DOMXPath($document);
-        $bodyNode = $xPath->query('//body')[0];
-        $bodyHtml = $cssToInline->convert($document->saveHTML($bodyNode), $css);
-
-        $doc2 = new \DOMDocument('1.0', 'UTF-8');
-        $doc2->loadHTML(mb_convert_encoding($bodyHtml, 'HTML-ENTITIES', 'UTF-8'));
-        $replacingNode = $doc2->getElementsByTagName('html')[0]->firstChild;
-
-        $replace = $document->importNode($replacingNode, true);
-        $bodyNode->parentNode->replaceChild($replace, $bodyNode);
-    }
+  "require": {
+      "DemigodCode/bootstrap-email": "master"
+  }
 }
+```
+
+or direct from [packagist](https://packagist.org/packages/DemigodCode/Bootstrap-Email)
+
+    composer require "DemigodCode/bootstrap-email: master"
+
+And run composer to update your dependencies:
+
+    $ curl -s http://getcomposer.org/installer | php
+    $ php composer.phar update
+
+## Usage
+
+You can use different methods to convert your boostrap-email html to an email client compatible html.
+
+### Use DomDocument
+
+```php
+    // Create a DOM Document
+    $doc = new DOMDocument('1.0', 'UTF-8');
+    \libxml_use_internal_errors(true);
+    $doc->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+    \libxml_clear_errors();
+    
+    $converter = new Compiler($scss);
+    $doc = $converter->convert($doc);
+    $html = $doc->saveHTML();
+```
+
+### Use Html
+
+```php
+    $converter = new Compiler($scss);
+    $html = $converter->convertHtml(html);
+```
+
+## Documentations
+For full documentation, visit [bootstrapemail.com](https://bootstrapemail.com/docs/introduction)
+
+## Community
+For help, discussion about best practices, or any other conversation that would benefit from being searchable:
+
+[Discuss Bootstrap Email on GitHub](https://github.com/bootstrap-email/bootstrap-email/discussions)
+
+For PHP Port related questions, please open an issue.
