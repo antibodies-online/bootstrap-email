1a26ae1cccd438ac074f8a0710f2c770b58b2681
Federico Sonetti <federico.sonetti@tecnomodel-treni.it>
2024-06-03 11:53:04 +0200

    add nested table for padding class

--- src/Converter/Padding.php
+++ src/Converter/Padding.php
@@ -7,7 +7,7 @@ class Padding extends AbstractConverter
     protected $xPathsOptions = [
         '//*[contains(concat(" ", @class, " "), " p-") or contains(concat(" ", @class, " "), " pt-") or contains(concat(" ", @class, " "), " pr-")'
         . ' or contains(concat(" ", @class, " "), " pb-") or contains(concat(" ", @class, " "), " pl-") or contains(concat(" ", @class, " "), " px-")'
-        . ' or contains(concat(" ", @class, " "), " py-")]'=> 'padding',
+        . ' or contains(concat(" ", @class, " "), " py-")]' => 'padding',
     ];
 
     private $paddingRegex = '/(p[trblxy]?-(lg-)?\d+)/';
@@ -21,11 +21,27 @@ class Padding extends AbstractConverter
     {
         $classes = [];
         preg_match_all($this->paddingRegex, parent::extractParentCssClasses($element, $identifier), $classes);
-        return parent::extractParentCssClasses($element, $identifier) . ' w-full';
+        return implode(' ', $classes[0]);
+    }
+
+    protected function buildChildNodeContent(\DOMElement $element, string $identifier): string
+    {
+        if ($element->tagName !== 'table' && $element->tagName !== 'td') {
+            $classes = [];
+            preg_match_all($this->paddingRegex, $element->getAttribute('class'), $classes);
+            foreach ($classes as $class) {
+                if (!empty($class[0]))
+                    $this->removeClass($element, $class[0]);
+            }
+        }
+        return $element->ownerDocument->saveHTML($element);
     }
 
     public function buildReplacementHtml(\DOMElement $element, string $identifier): string
     {
+        if ($element->tagName !== 'table' && $element->tagName !== 'td') {
+            return parent::buildReplacementHtml($element, $identifier);
+        }
         return $element->ownerDocument->saveHTML($element);
     }
 
@@ -33,5 +49,4 @@ class Padding extends AbstractConverter
     {
         return in_array($element->tagName, ['table', 'td', 'a']);
     }
-
 }
